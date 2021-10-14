21c5baeaf7090ee562d2d793d643530146f726f7
justinuni <justinuni@users.noreply.github.com>
2021-10-14 21:00:32 +1000

    Carpark rules

--- carpark_rules.h
+++ carpark_rules.h
@@ -5,4 +5,21 @@
 #define EXITS (5)
 #define LEVELS (5)
 
+#define MIN_CAR_SPAWN (1)
+#define MAX_CAR_SPAWN (100)
+#define LPR_QUEUE_WAIT (2)
+#define BOOMGATE_OPEN_TIME (10)
+#define BOOMGATE_CLOSE_TIME (10)
+#define CAR_PARKING_TIME (10)
+#define MIN_CAR_PARKED (100)
+#define MAX_CAR_PARKED (10000)
+#define CAR_LEAVING_TIME (10)
+
+#define MIN_TEMP_CHANGE_TIME (1)
+#define MAX_TEMP_CHANGE_TIME (5)
+
+#define BOOMGATE_OPENED_TIME (20)
+
+#define TEMP_CHECK_TIME (2)
+
 #endif
