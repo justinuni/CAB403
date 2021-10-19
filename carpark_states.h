fedca307f74c8c03b10616ffdf31d808e1ebb759
justinuni <justinuni@users.noreply.github.com>
2021-10-19 12:37:19 +1000

    Carpark states header

---
+++ carpark_states.h
@@ -0,0 +1,10 @@
+#ifndef CARPARK_STATES
+#define CARPARK_STATES
+
+#define SIGN_EMPTY (0)
+#define BOOMGATE_OPENING ('o')
+#define BOOMGATE_OPENED ('O')
+#define BOOMGATE_CLOSING ('c')
+#define BOOMGATE_CLOSED ('C')
+
+#endif
