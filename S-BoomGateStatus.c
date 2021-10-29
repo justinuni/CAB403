7dc36dbf88d9fd6cc9255c2b9bf233eaeb82b030
Username1950sadsadas <91536890+Username1950sadsadas@users.noreply.github.com>
2021-10-29 08:58:50 +0300

    Update S-BoomGateStatus.c

--- S-BoomGateStatus.c
+++ S-BoomGateStatus.c
@@ -4,50 +4,46 @@
 #include <stdbool.h>
 #include <pthread.h>
 #include <stdio.h>
+#include "carpark_types.h"
+#include "carpark_states.h"
+#include "carpark_rules.h"
+
 volatile void *simulator_shm;
 
-typedef struct boom_gate_simulation boom_gate_simulation_t;
-struct boom_gate_simulation {
-	pthread_mutex_t m;
-	pthread_cond_t c;
-	char s;
-};
 void *boom_gate_controller_entry(int level){
     int addr = 288 * level + 96;
-	volatile struct boom_gate_simulation *bg = simulator_shm + addr;
-
+	volatile struct boomgate_t *bg = simulator_shm + addr; 
     
-    bg->s = 'C';
+    bg->status = 'C';
 
     // main while loop
     while(1){
-        if (bg->s == 'R') {
+        if (bg->status == 'R') {
             sleep(1);
 
-			bg->s = 'O';
+			bg->status = 'O';
 		} else if (bg->s == 'L'){
             usleep(10000);
-			bg->s = 'C';
+			bg->status = 'C';
         }
     }
 }
 
 void *boom_gate_controller_exit(int level){
     int addr = 192 * level + 1536;
-	volatile struct boom_gate_simulation *bg = simulator_shm + addr;
-
+	volatile struct boomgate_t *bg = simulator_shm + addr;
     
-    bg->s = 'C';
+    bg->status = 'C';
 
     // main while loop
     while(1){
-        if (bg->s == 'R') {
+        if (bg->status == 'R') {
             sleep(1);
-			bg->s = 'O';
+			bg->status = 'O';
 
 		} else if (bg->s == 'L'){
             usleep(10000);
-			bg->s = 'C';
+			bg->status = 'C';
         }
     }
 }
@@ -59,3 +55,4 @@ void *boom_gate_controller_exit(int level){
 	for (int i = 0; i < SIMULATOR_EXITS; i++) {
 		pthread_create(boomgatethreads + SIMULATOR_ENTRANCES + i, NULL, (void *(*)(void *)) boom_gate_controller_exit, (void*) (intptr_t) i);
 	}
+
