475fd2d755aeded2e6b7a5fa1d49156fffef0fa9
James Weidinger <jamesweidinger1@gmail.com>
2021-11-01 17:05:01 +1000

    Updated status display to actually print values

--- status.c
+++ status.c
@@ -6,14 +6,12 @@
 #include "carpark_types.h"
 #include "carpark_states.h"
 #include "carpark_rules.h"
+#include "manager.h"
 
 #include "manager.c"
 
 void status_control() {
-    while (1) {
-        display_status();
-        usleep(50000);
-    }
+    display_status();
 }
 
 void display_status() {
@@ -28,61 +26,68 @@ void display_status() {
     exit_t *exit_data;
     char *exit_lpr_contents;
     char exit_boom_status;
-    char exit_sign_status;
 
     level_t *level_data;
     char *level_lpr_contents;
 
-    for (int i = 0; i < ENTRANCES; i++) {
-        get_entrance(shm, i, &entrance_data);
-        read_lpr(&entrance_data->lpr, entrance_lpr_contents);
-        entrance_boom_status = read_boom(&entrance_data->boomgate);
-        entrance_sign_status = read_sign(&entrance_data->sign);
-        print_entrance();
+    while (1) {
+        write_border();
+
+        for (int i = 0; i < ENTRANCES; i++) {
+            get_entrance(shm, i, &entrance_data);
+            read_lpr(&entrance_data->lpr, entrance_lpr_contents);
+            entrance_boom_status = read_boom(&entrance_data->boomgate);
+            entrance_sign_status = read_sign(&entrance_data->sign);
+            print_entrance(i, entrance_lpr_contents, entrance_boom_status, entrance_sign_status);
+        }
+
+        for (int i = 0; i < EXITS; i++) {
+            get_exit(shm, i, &exit_data);
+            read_lpr(&exit_data->lpr, exit_lpr_contents);
+            exit_boom_status = read_boom(&exit_data->boomgate);
+            print_exit(i, exit_lpr_contents, exit_boom_status);
+        }
+
+        for (int i = 0; i < LEVELS; i++) {
+            get_level(shm, i, &level_data);
+            read_lpr(&level_data->lpr, level_lpr_contents);
+            print_level(i, level_lpr_contents);
+
+            print_level_capacities(i);
+        }
+
+        print_revenue();
+
+        write_border();
+
+        usleep(50000);
+        system("clear");
     }
-
-    for (int i = 0; i < EXITS; i++) {
-        get_exit(shm, i, &exit_data);
-        read_lpr(&exit_data->lpr, exit_lpr_contents);
-        exit_boom_status = read_boom(&exit_data->boomgate);
-        print_exit();
-    }
-
-    for (int i = 0; i < LEVELS; i++) {
-        get_level(shm, i, &level_data);
-        read_lpr(&level_data->lpr, level_lpr_contents);
-        print_level();
-    }
-
-    system("clear");
 }
 
 
-void write_border(int block_side) {
-    if (!block_side) {
-        printf("================================================================================================\n");
-        
-    } 
-    else printf("================================================================================================\n");
+void write_border() {
+    printf("================================================================================================\n");
 }
 
-void print_entrance() {
-    printf("Entrance information");
+void print_entrance(int number, char *entrance_lpr_contents, char entrance_boom_status, char entrance_sign_status) {
+    entrance_lpr_contents[6] = '\0';
+    printf("ENTRANCE %c || LPR: %s || BOOM: %c || SIGN: %c\n", number, *entrance_lpr_contents, entrance_boom_status, entrance_sign_status);
 }
 
-void print_exit() {
-    printf("Exit information");
+void print_exit(int number, char *exit_lpr_contents, char exit_boom_status) {
+    printf("EXIT %c || LPR: %s || BOOM: %c\n", number, *exit_lpr_contents, exit_boom_status);
 }
 
-void print_level() {
+void print_level(int number, char *level_lpr_contents) {
     printf("Level information");
+    printf("EXIT %c || LPR: %s\n", number, *level_lpr_contents);
 }
 
-
-void print_details() {
-    
+void print_level_capacities(int level_num) {
+    printf("LEVEL %d CAPACITY: %d\n", level_num, level_capacities[level_num]);
 }
 
-
-
-//printf("||         || CAPACITY || ENTRANCE (LPR) || ENTRANCE (BOOM) || EXIT (LPR) || EXIT (BOOM) || SIGN DISPLAY ||\n");
+void print_revenue() {
+    printf("TOTAL REVENUE: %d\n", total_revenue);
+}
